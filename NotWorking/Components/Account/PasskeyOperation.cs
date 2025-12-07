25778835dbd91a07447993d7a0548aeccf4a26b8
Patrice Charbonneau <141130623+Sigma3Wolf@users.noreply.github.com>
2025-12-07 08:09:33 -0500

    Init

---
+++ NotWorking/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace NotWorking.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
---
+++ NotWorking/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace NotWorking.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
